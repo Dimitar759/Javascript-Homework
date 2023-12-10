b65854bfe93a0a4d8571a06bf5d312d1b8e1c30a
Dimitar759 <142807026+Dimitar759@users.noreply.github.com>
2023-12-10 17:34:04 +0100

    New problem added
    Solved the second problem of the homework - dog and human years

--- Class3 Homework/script.js
+++ Class3 Homework/script.js
@@ -24,3 +24,44 @@ console.log(typeObject);
 
 let typeUndefined = returningType();
 console.log(typeUndefined);
+
+
+//a JavaScript function that Calculates your dog's age based on the conversion rate of 1 human year to 7 dog years
+
+function humanAndDogYears(years) {
+    let convertingNumber = 7;
+    let dogAge = years * convertingNumber;
+    let humanAge =years / convertingNumber ;
+
+    return `The human's age in dog years is ${dogAge} and the dog's age in human years is ${humanAge}`;
+}
+
+let enterAge = parseFloat(prompt("Enter years:"));
+
+const humanAndDogAge = humanAndDogYears(enterAge);
+console.log(humanAndDogAge)
+
+
+
+/////////////////
+
+
+// function calculateDogYears(yearsToConvert, convertRatio) {
+//     let dogYears = yearsToConvert * convertRatio;
+//     return dogYears;
+// }
+
+// function calculateHumanYears(yearsToConvert, convertRatio) {
+//     let humanYears = yearsToConvert / convertRatio;
+//     return humanYears;
+// }
+
+// let yearsToConvert = parseFloat(prompt("Enter years:"))
+
+// let convertRatio = 7;
+// const finalDogResult = calculateDogYears(yearsToConvert, convertRatio);
+
+// const finalHumanResult = calculateHumanYears(yearsToConvert, convertRatio);
+
+// console.log("In dog years is:", finalDogResult);
+// console.log("In human years is:", finalHumanResult);
