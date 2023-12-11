52de188f6319bb22bdfb8913ebf108099ba40c49
Dimitar759 <142807026+Dimitar759@users.noreply.github.com>
2023-12-12 00:15:32 +0100

    Problem 2 solved

--- Class4 Homework/script.js
+++ Class4 Homework/script.js
@@ -1,10 +1,11 @@
+// story function
 
 function tellStory(storyWords) {
     let name;
     let emotion;
     let activity;
 
-    for (let i = 0; i < storyWords.length; i++) {
+    for (let i = 0; i < storyWords.length; i++){
         if (storyWords[i] === "Ben") {
              name = storyWords[i];
         }
@@ -14,10 +15,25 @@ function tellStory(storyWords) {
         else if (storyWords[i] === "Running") {
              activity = storyWords[i];
         }
+        
     }
 
-    console.log(`This is ${name}. ${name} is a nice person. Today they are ${emotion}. They are ${activity} all day. The end.`);
+    console.log(` This is ${name}. ${name} is a nice person. Today they are ${emotion}. They are ${activity} all day. The end.`);
 }
 
-let storyValues = ["Ben", "Happy", "Running"];
-tellStory(storyValues);
+let story = ["Ben", "Happy", "Running"];
+tellStory(story);
+
+
+//Sum of 5 numbers
+
+function sumOfNumbers(allNumbers) {
+    let sum = 0;
+    for (let i = 0; i < allNumbers.length; i++){
+        sum += allNumbers[i];
+    }
+    console.log(sum);
+}
+
+let numbers = [12, 64, 987, 543, 78];
+sumOfNumbers(numbers);
