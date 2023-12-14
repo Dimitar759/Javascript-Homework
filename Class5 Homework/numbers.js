e860c39fbe2e3ae3547e6f452fd485b9b2074911
Dimitar759 <142807026+Dimitar759@users.noreply.github.com>
2023-12-14 19:04:01 +0100

    Updates
    Updated the second problem with functions

--- Class5 Homework/numbers.js
+++ Class5 Homework/numbers.js
@@ -1,23 +1,33 @@
 // part 2
 
-let numbers = [1, 2, 4, 5, 7, 8, 12, 21];
-let list = document.getElementById("list");
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    list.innerHTML += "<li>" + numbers[i] + "</li>";
-    sum += numbers[i];
+function printingAndSum(numbers) {
+    let list = document.getElementById("list");
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        list.innerHTML += "<li>" + numbers[i] + "</li>";
+        sum += numbers[i];
+    }
+    list.innerHTML += "<li>" + `Sum of all numbers is: ${sum}` + "</li>";
+    return sum;
 }
-list.innerHTML += "<li>" + `Sum of all numbers is: ${sum}` + "</li>";
+
 
 
 let equation = document.getElementById("equation");
 let equationString = '';
-
-for (let i = 0; i < numbers.length; i++) {
+function printingTheEquation(numbers, sum) {
+    for (let i = 0; i < numbers.length; i++) {
     equationString += `${numbers[i]}`;
-    if (i !== numbers.length - 1) {
+        if (i !== numbers.length - 1) {
         equationString += ' + ';
+        }
     }
+
+    equation.innerHTML = equationString + ` = ${sum}`;
+    return equation;
 }
 
-equation.innerHTML = equationString + ` = ${sum}`;
+let numbers = [1, 2, 4, 5, 7, 8, 12, 21];
+let totalSum = printingAndSum(numbers);
+printingTheEquation(numbers, totalSum);
+
