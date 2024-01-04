e1df36fcadf99140ac1ff37f86278163ecb1eccd
Dimitar759 <142807026+Dimitar759@users.noreply.github.com>
2024-01-04 21:10:58 +0100

    Updates to the css and html

--- Winter Homework Hangman/script.js
+++ Winter Homework Hangman/script.js
@@ -10,10 +10,26 @@ let sportsButton = document.getElementById("sports");
 
 let topicDiv = document.getElementById("choosing-topic"); 
 let inputContainer = document.getElementById("input-container"); 
+let infoParagraph = document.getElementById("info-paragraph"); 
 
 
 moviesButton.addEventListener("click", function () {
     topicDiv.style.display = "none";
     let chosenWord = movies[Math.floor(Math.random() * movies.length)];
+    infoParagraph.innerHTML += `The chosen category is Movies `
+    inputContainer.classList.remove("input-container");
+})
+
+showsButton.addEventListener("click", function () {
+    topicDiv.style.display = "none";
+    let chosenWord = shows[Math.floor(Math.random() * shows.length)];
+    infoParagraph.innerHTML += `The chosen category is Shows `
+    inputContainer.classList.remove("input-container");
+})
+
+sportsButton.addEventListener("click", function () {
+    topicDiv.style.display = "none";
+    let chosenWord = sports[Math.floor(Math.random() * sports.length)];
+    infoParagraph.innerHTML += `The chosen category is Sports `
     inputContainer.classList.remove("input-container");
 })
